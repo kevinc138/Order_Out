5bd77570ff7834cfdfacf9812251dd59fc9f83e9
DePauw Open Source Development <kevin.courtade@hotmail.com>
2015-04-13 17:48:04 -0400

    Added restaurantId and admin fields to users and began implementing functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150413163612) do
+ActiveRecord::Schema.define(version: 20150413213022) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 20150413163612) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin"
+    t.integer  "restaurantid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
